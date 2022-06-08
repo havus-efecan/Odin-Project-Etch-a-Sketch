d950d061852d534d04525292f1e90f4b0ae95365
Efecan Havus <Ehavus@gmail.com>
2022-06-08 15:31:23 -0400

    creates grid of x by x size

---
+++ EtchJS.js
@@ -0,0 +1,44 @@
+
+    let size = 16
+
+
+    for (j = 0; j < size; j++){
+
+
+        let box = document.createElement('div');
+        box.classList.add('box')
+        document.body.appendChild(box);
+
+
+
+        for (i = 0; i < size; i++){
+
+
+            let square = document.createElement('div')
+         
+         
+            square.classList.add('square1')
+         
+            box.appendChild(square)
+        
+        
+        
+        }
+
+
+
+
+
+    }
+
+
+  
+
+
+
+
+
+
+  
+
+

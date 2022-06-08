3fc2cf4ac80177b18f4609170734fae67fc6f119
Efecan Havus <Ehavus@gmail.com>
2022-06-08 16:36:09 -0400

    squares become random color on mouse hover

--- EtchJS.js
+++ EtchJS.js
@@ -2,13 +2,29 @@
     let size = 16
 
 
+
+
+    
+    
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+
+    
+
+
+
     for (j = 0; j < size; j++){
 
 
         let box = document.createElement('div');
         box.classList.add('box')
         document.body.appendChild(box);
-
 
 
         for (i = 0; i < size; i++){
@@ -31,14 +47,37 @@
 
     }
 
+    let squareArray = document.getElementsByClassName('square1')
 
+    // squareArray[0].addEventListener('mouseover',hover)
+    // squareArray[0].addEventListener('mouseout',unHover)
+
+
+     for(x = 0; x < squareArray.length;x++){
+
+
+     squareArray[x].addEventListener('mouseover',hover)
+   // squareArray[x].addEventListener('mouseout',unHover)
+
+     }
+
+      squareArray.item(0)
+     
+
+   function hover(){
+
+        
+    //   this.classList.add('highlighted')
+       this.style.backgroundColor = getRandomColor();
+
+   }
   
+   function unHover(){
+
+        
+   // this.classList.remove('highlighted')
 
 
 
-
-
-
-  
-
+}
 
